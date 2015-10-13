38f2601bb095adcc638e163b3812eb2cf5918f85
ziyangzeng <zz2368@columbia.edu>
2015-10-13 15:22:46 -0400

    index change

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index_finish.html")
+    return render_template("index.html")
 
 @app.route("/getData/")
 def getData():
